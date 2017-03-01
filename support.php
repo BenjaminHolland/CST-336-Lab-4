fe89c02b2027e44c28d423887e93f9a5048ac723
Ben Holland <bholland@csumb.edu>
2017-03-01 22:49:07 +0000

    Fixed incorrect item header

--- support.php
+++ support.php
@@ -37,18 +37,22 @@
                 </div>
                 <div id='support_divMerch'>
                     <div id='support_divMugItem' class='support_ItemLine'>
-                        <input id='support_cbxMug' class='support_itemEnabler' name='orderedMug'  type='checkbox'>Mug</input>
-                        <input id='support_colMug' class='support_itemColorer' name='mugColor' type='color'>Mug Color</input>
+                        <div id='support_lblMugEnable'>Mug</div>
+                        <input id='support_cbxMug' class='support_itemEnabler' name='orderedMug'  type='checkbox'></input>
+                        <input id='support_colMug' class='support_itemColorer' name='mugColor' type='color'></input>
                         <div id='support_lblMugPrice' class='support_itemPrice'>$10</div>
                     </div>
                     <div id='support_divPenItem' class='support_ItemLine'>
-                        <input id='support_cbxPen' class='support_itemEnabler' name='orderedPen' type='checkbox'>Pen</input>
-                        <input id='support_colPen' class='support_itemColorer' name='penColor' type='color'>Pen Color</input>
+                        <div id='support_lblPenEnable'>Pen</div>
+                        <input id='support_cbxPen' class='support_itemEnabler' name='orderedPen' type='checkbox'></input>
+                        <input id='support_colPen' class='support_itemColorer' name='penColor' type='color'></input>
                         <div id='support_lblPenPrice' class='support_itemPrice'>$2</div>
                     </div>
                     <div id='support_divShirtItem' class='support_ItemLine'>
-                        <input id='support_cbxShirt' class='support_itemEnabler' name='orderedShirt' type='checkbox'>Pen</input>
-                        <input id='support_colShirt' class='support_itemColorer' name='shirtColor' type='color'>Pen Color</input>
+                        
+                        <div id='support_lblShirtEnable'>Shirt</div>
+                        <input id='support_cbxShirt' class='support_itemEnabler' name='orderedShirt' type='checkbox'></input>
+                        <input id='support_colShirt' class='support_itemColorer' name='shirtColor' type='color'>Shirt Color</input>
                         <div id='support_szShirt'>
                             <input id='support_szShirt_S' type='radio' name='shirtSize'  value='S'>Small</input>
                             <input id='support_szShirt_S' type='radio' name='shirtSize'  value='M' checked='checked'>Medium</input>
