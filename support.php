628bdd1acd312d01edb2142953e297c0dcf5015f
Ben Holland <bholland@csumb.edu>
2017-03-01 21:16:48 +0000

    Added Skeleton for landing page

---
+++ support.php
@@ -0,0 +1,19 @@
+<html>
+    <title>
+        
+    </title>
+    <body class='ContentContainer'>
+        <div class='Header'>
+            
+        </div>
+        <div class='ImageContainer'>
+            <!--TODO: Find better image-->
+            <img id="imgInspiration" src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_dark_color_272x92dp.png'/>
+        </div>
+        <div class='FormContainer'>
+            <form id='frmSupport' action='./confirm.php' method='POST'>
+                <button id='btnSubmit'>Submit</button>
+            </form>
+        </div>
+    </body>
+</html>
