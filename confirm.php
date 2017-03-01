1cdf9d3f903672f3dd8c73f5cf25018f49e342db
Ben Holland <bholland@csumb.edu>
2017-03-01 21:23:34 +0000

    added skeleton for confirmation page

---
+++ confirm.php
@@ -0,0 +1,19 @@
+<html>
+    <title>
+        Order Confirmation
+    </title>
+    <body id="confirm_body">
+        <div id="confirm_divItemListHeader">Items</div>
+        <div id="confirm_divItemList">
+            <?PHP
+                //Display list of purchased items here.
+            ?>
+        </div>
+        <div id="confirm_divOrderTotal">
+            <div id="confirm_lblOrderTotal">Order Total</div>
+            <?PHP
+                //Display order total;
+            ?>
+        </div>
+    </body>
+</html>
